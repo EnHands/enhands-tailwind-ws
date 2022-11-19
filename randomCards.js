5ed04d019b621b76bf74ed30c1e76fbe15d885cf
LeonKie <leon.kiesgen@web.de>
2022-11-19 15:50:02 +0100

    update index.html and randomCards.js

--- randomCards.js
+++ randomCards.js
@@ -3,7 +3,24 @@
 card = document.getElementsByClassName("card")
 cardList= document.getElementById("CardList")
 
+fetch('./people.json')
+    .then((response) => response.json())
+    .then((people) => 
+    {
+        console.log(people)
+        people.sort((a, b) => 0.5 - Math.random());
 
+        people.forEach(element => {
+            var T=`<div class="card w-60 rounded overflow-hidden shadow-lg"><img class="w-full" src="images/people/${element.img}" alt="Mountain"><div class="px-6 py-4"><div class="font-bold text-xl mb-2">${element.name}</div><p class="text-gray-700 text-base">${element.job}</p></div></div>`
+            var temp = document.createElement('div');
+            temp.innerHTML = T;
+            var htmlObject = temp.firstChild;
+            console.log(htmlObject)
+            cardList.append(htmlObject)
+
+    });
+
+```
 var people=[
     {name: "Johannes Frey",
     img: "JohannesFrey-MA-RCI.jpg",
@@ -32,20 +49,10 @@ var people=[
     {name: "Amartya Ganguly, Ph.D.",
     img: "AmartyaGanguly.png",
     job: "Biomechanics, Munich Institute of Robotics and Machine Intelligence (MIRMI)"},
-
-
 ]
-console.log(people)
 
-people.sort((a, b) => 0.5 - Math.random());
+```
 
-people.forEach(element => {
-    var T=`<div class="card w-60 rounded overflow-hidden shadow-lg"><img class="w-full" src="images/people/${element.img}" alt="Mountain"><div class="px-6 py-4"><div class="font-bold text-xl mb-2">${element.name}</div><p class="text-gray-700 text-base">${element.job}</p></div></div>`
-    var temp = document.createElement('div');
-    temp.innerHTML = T;
-    var htmlObject = temp.firstChild;
-    console.log(htmlObject)
-    cardList.append(htmlObject)
 });
 
 
