413933403a2befcfa4f9ae93beb834438b993960
Vuenc <v.buergin@gmx.de>
2023-01-15 21:53:26 +0100

    Fixed shuffling of random cards (was biased towards the initial ordering)

--- randomCards.js
+++ randomCards.js
@@ -7,9 +7,14 @@ fetch('./people.json')
     .then((response) => response.json())
     .then((people) => 
     {
-        console.log(people)
-        people.sort((a, b) => 0.5 - Math.random());
-
+        function shuffleArray(array) {
+            for (let i = array.length - 1; i > 0; i--) {
+                const j = Math.floor(Math.random() * (i + 1));
+                [array[i], array[j]] = [array[j], array[i]];
+            }
+        }
+        shuffleArray(people);
+        
         people.forEach(element => {
             var T=`<div class="card w-36 sm:w-60 rounded overflow-hidden shadow-lg"><img class="w-full max-h-60 object-cover" src="${element.img}" alt="Mountain"><div class="px-3 sm:px-6 py-4"><div class="font-bold text-sm sm:text-xl mb-2">${element.name}</div><p class="text-gray-700 text-xs sm:text-base">${element.job}</p></div></div>`
             var temp = document.createElement('div');
