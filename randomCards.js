a766b120db2c66e5663988ecc30502ee1b14d7a3
Vuenc <v.buergin@gmx.de>
2023-08-01 13:10:22 +0200

    Cleaned randomCards.js

--- randomCards.js
+++ randomCards.js
@@ -16,13 +16,13 @@ fetch('./people.json')
         shuffleArray(people);
         
         people.forEach(element => {
-            var T=`<div class="card w-36 sm:w-60 rounded overflow-hidden shadow-lg"><img class="w-full max-h-60 object-cover" src="${element.img}" alt="Mountain"><div class="px-3 sm:px-6 py-4"><div class="font-bold text-sm sm:text-xl mb-2">${element.name}</div><p class="text-gray-700 text-xs sm:text-base">${element.job}</p></div></div>`
+            var T=`<div class="card w-36 sm:w-60 rounded overflow-hidden shadow-lg"><img class="w-full max-h-60 object-cover" src="${element.img}"alt="Team member">
+                <div class="px-3 sm:px-6 py-4"><div class="font-bold text-sm sm:text-xl mb-2">${element.name}</div>
+                <p class="text-gray-700 text-xs sm:text-base">${element.job}</p></div></div>`
             var temp = document.createElement('div');
             temp.innerHTML = T;
             var htmlObject = temp.firstChild;
-            console.log(htmlObject)
             cardList.append(htmlObject)
-
     });
 
 
