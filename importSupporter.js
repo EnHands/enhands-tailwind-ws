2fa02e07ea1ba508819253a96dce52de017f7900
Leon Kie <leon.kiesgen@web.de>
2023-11-24 13:48:36 +0100

    adding supporter  feature to new logo cloud

--- importSupporter.js
+++ importSupporter.js
@@ -1,9 +1,9 @@
 
 
 // This file is used to generate random cards for the team section of the website
-cardList= document.getElementById("CardList-People")
+cardList_Partner= document.getElementById("CardList-Partner")
 
-fetch('./people.json')
+fetch('./supporter.json')
     .then((response) => response.json())
     .then((people) => 
     {
@@ -19,10 +19,12 @@ fetch('./people.json')
             var T=`<div class="card w-36 sm:w-60 rounded overflow-hidden shadow-lg"><img class="w-full max-h-60 object-cover" src="${element.img}"alt="Team member">
                 <div class="px-3 sm:px-6 py-4"><div class="font-bold text-sm sm:text-xl mb-2">${element.name}</div>
                 <p class="text-gray-700 text-xs sm:text-base">${element.job}</p></div></div>`
+
+            var Text = `<img class="col-span-2 max-h-16 w-full object-contain lg:col-span-1" src="${element.img}" alt="Transistor" width="158" height="52"></img>`    
             var temp = document.createElement('div');
-            temp.innerHTML = T;
+            temp.innerHTML = Text;
             var htmlObject = temp.firstChild;
-            cardList.append(htmlObject)
+            cardList_Partner.append(htmlObject)
     });
 
 
