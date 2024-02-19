85d77169a8d63d3c7bde02ffeb65d1da4058225c
Vincent Bürgin <vincent.buergin@tum.de>
2024-02-19 15:12:27 +0100

    Take new profile pictures life, add Amartya

--- randomCards.js
+++ randomCards.js
@@ -16,11 +16,19 @@ fetch('./people.json')
         shuffleArray(people);
         
         people.forEach(element => {
-            var T=`<div class="card w-36 sm:w-60 rounded overflow-hidden shadow-lg"><img class="w-full max-h-60 object-cover" src="${element.img}"alt="Team member">
+            let hoverImg = element.img_hover ? `<img class="img-hover w-full max-h-60 object-cover" src="${element.img_hover}"alt="${element.name} portrait">` : "";
+            var T = `<div class="card w-36 sm:w-60 rounded overflow-hidden shadow-lg">
+                <div class="card-image-area">
+                <img class="${hoverImg ? 'img-nohover' : ''} w-full max-h-60 object-cover" src="${element.img}"alt="${element.name} portrait">
+                ${hoverImg}
+                </div>
                 <div class="px-3 sm:px-6 py-4"><div class="font-bold text-sm sm:text-xl mb-2">${element.name}</div>
-                <p class="text-gray-700 text-xs sm:text-base">${element.job}</p></div></div>`
+                <p class="text-gray-700 text-xs sm:text-base">${element.job}</p></div></div>`;
             var temp = document.createElement('div');
             temp.innerHTML = T;
+            for (let imageElement of temp.getElementsByTagName("img")) {
+                imageElement.decode();
+            }
             var htmlObject = temp.firstChild;
             cardList.append(htmlObject)
     });
